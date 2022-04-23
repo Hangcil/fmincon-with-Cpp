9899e836bac3cf11b27946ee16179e2a2a20a1a4
Hangcil <90510101+Hangcil@users.noreply.github.com>
2022-04-24 00:03:16 +0800

    Update sci_arma.h

--- include/sci_arma.h
+++ include/sci_arma.h
@@ -3,10 +3,11 @@
 
 #include "armadillo"
 #include <functional>
+#include <algorithm>
 using namespace arma;
 using obj_fun=std::function<double(vec&)>;
 
-//x_fval performs as a wrapper for the returned value.
+//x_fval defines local solution and minimal value
 struct x_fval
 {
 public:
@@ -14,12 +15,12 @@ public:
     double fval;
 };
 
-//two simple static member functions
+//class sci_arma wraps two functions
 class sci_arma
 {
 public:
     static x_fval fmin(const obj_fun& f, vec& x0);
-    static x_fval fmincon(const obj_fun& f, vec& x0, mat& Aeq, mat& beq);
+    static x_fval fmincon(const obj_fun& f, vec& x0, mat& A, mat& b);
 };
 
 #endif //ARM_TEST_SCI_ARMA_H
