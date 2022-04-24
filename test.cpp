4404e4faa371695407da5ff9e8f6090d6be04a3f
Hangcil <90510101+Hangcil@users.noreply.github.com>
2022-04-25 00:33:52 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,26 +6,48 @@ int main() {
     auto f=[](vec& x)-> double {
         return pow(1-x(0),2)+pow(x(1)-x(0)*x(0),2);
     };
-    
+
     //define a start point
     vec x0= zeros(2,1);
-    
+
     //optimization with no constraints
     auto result1=sci_arma::fmin(f,x0);
-    
+
     //define linear constraints Ax<=b
     mat A={{1,1},{2,3}};
     vec b={1,2};
-    
-    //optimization with constraints
+
+    //optimization with inequality linear constraints
     auto result2= sci_arma::fmincon(f, x0, A, b);
-    
+
+    //define linear equality constraints
+    mat Aeq={{1,2}};
+    vec beq={0};
+
+    //optimization with mixed linear constraints
+    auto result3= sci_arma::fmincon(f, x0, A, b, Aeq, beq);
+
+    //define non-linear inequality constraints c(x)<=0
+    auto c=[](vec& x)->vec {
+        vec temp={x(0)*x(0)+x(1)*x(1)-0.05};
+        return temp;
+    };
+
+    //optimization with mixed constraints
+    //para '1' tells that there is only one non-linear constraint
+    auto result4= sci_arma::fmincon(f, x0, A, b, Aeq, beq, c, 1);
+
     //output
     std::cout<<"empty constraints:" <<endl<<"local solution: "<<endl
-    <<result1.x<<"min:"<<endl<<"  "<<result1.fval<<endl;
+             <<result1.x<<"min:"<<endl<<"  "<<result1.fval<<endl;
     std::cout<<endl;
     std::cout<<"linear constraints:"<<endl<<"local solution: "<<endl
-    <<result2.x<<"min:"<<endl<<"  "<<result2.fval<<endl;
-    
+             <<result2.x<<"min:"<<endl<<"  "<<result2.fval<<endl;
+    std::cout<<endl;
+    std::cout<<"Mixed linear constraints:"<<endl<<"local solution: "<<endl
+             <<result3.x<<"min:"<<endl<<"  "<<result3.fval<<endl;
+    std::cout<<endl;
+    std::cout<<"Mixed constraints:"<<endl<<"local solution: "<<endl
+             <<result4.x<<"min:"<<endl<<"  "<<result4.fval<<endl;
     return 0;
 }
