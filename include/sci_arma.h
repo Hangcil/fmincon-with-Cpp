2a9bfe63a874d301b93c1365d0c6595702fb976e
Hangcil <90510101+Hangcil@users.noreply.github.com>
2022-04-25 00:37:20 +0800

    Update sci_arma.h

--- include/sci_arma.h
+++ include/sci_arma.h
@@ -5,9 +5,12 @@
 #include <functional>
 #include <algorithm>
 using namespace arma;
-using obj_fun=std::function<double(vec&)>;
 
-//x_fval defines local solution and minimal value
+//defines special functions needed when optimizing
+using obj_fun=std::function<double(vec&)>;
+using non_linear_con=std::function<vec(vec&)>;
+
+//x_fval defines local minimal returned by fmincon()
 struct x_fval
 {
 public:
@@ -15,12 +18,14 @@ public:
     double fval;
 };
 
-//class sci_arma wraps two functions
+//sci_arma wraps optimizing functions
 class sci_arma
 {
 public:
     static x_fval fmin(const obj_fun& f, vec& x0);
     static x_fval fmincon(const obj_fun& f, vec& x0, mat& A, mat& b);
+    static x_fval fmincon(const obj_fun& f, vec& x0, mat& A, mat& b, mat& Aeq, mat& beq);
+    static x_fval fmincon(const obj_fun& f, vec& x0, mat& A, mat& b, mat& Aeq, mat& beq, const non_linear_con& c, long n_non_lin);
 };
 
 #endif //ARM_TEST_SCI_ARMA_H
